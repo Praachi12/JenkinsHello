81d35d8076a7e340fc51efc701601941b27ba838
Prachi <kothawade.prachi12@gmail.com>
2023-03-16 17:20:04 -0500

    First Jenkins

---
+++ JenkinsHello/src/com/perscholas/jenkins_hello/HelloJenkins.java
@@ -0,0 +1,21 @@
+package com.perscholas.jenkins_hello;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			System.out.println(helloJenkins());
+			System.out.println(helloJenkinsAgain());
+		}
+		
+		public static String helloJenkins() {
+			return "Hello Jenkins!";
+		}
+		
+		public static String helloJenkinsAgain() {
+			return "Hello Jenkins again!!!";
+		}
+	}
+
+
